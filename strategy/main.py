928fe05f2be84f44461d2840e32eb636c4ea96ad
rhiegen <rhiegen@gmail.com>
2022-04-19 20:33:35 -0300

    refactoring override mounting

--- strategy/main.py
+++ strategy/main.py
@@ -1,82 +1,36 @@
 from pattern_chain import *
 
+def get_override(ovr_existente:str,ovr_gerado:str)-> str:
+    resultado = ''
+    __chain = None
+    __chain2 = None
+    
+    __chain = Unit_chain()
+    __chain2 = Clas_chain()
+    __chain.set_proximo(__chain2)
+
+    lista_gerada = __chain.get_parameters(ovr_gerado,[])
+    lista_existente = __chain.get_parameters(ovr_existente,[])
+    resultado = __chain.montar_override(lista_gerada, lista_existente, resultado)
+    
+    #cria texto final do override
+    indice = ovr_gerado.find(' DD')
+    cab = ovr_gerado[0:indice+4]
+    override = cab +  resultado
+    return override
+        
+        
+        
 
 
-def get_parameter(__ovr:str)-> List:
-        __chain = None
-        __chain2 = None
-        __chain = Unit_chain()
-        __chain2 = Clas_chain()
-        __chain.set_proximo(__chain2)
-
-        return __chain.get_parameters(__ovr,[])
 
 
         
 if __name__ == "__main__":
-    # The client code picks a concrete strategy and passes it to the context.
-    # The client should be aware of the differences between strategies in order
-    # to make the right choice.
-
-    ovr_gerado = 'STEP.DDNAME DD UNIT=(SYSDA,25),DATACLAS=XDEFAULT,STORCLAS=EXTNOST3'
-    ovr_existente = 'STEP.DDNAME DD UNIT=(SYSDA,25)'
-
-    
-
-
-    
-
-
-    # chain = Unit_chain()
-    # chain2 = Clas_chain()
-    # chain.set_proximo(chain2)
-
-
-    # lista_gerada = chain.get_parameters(ovr_gerado)
-    lista_gerada = get_parameter(ovr_gerado)
-    print(f'Lista gerada {lista_gerada}')
-    lista_existente = get_parameter(ovr_existente)
-
-
-    # lista_existente = chain.get_parameters(ovr_existente)
-    print(f'Lista existente {lista_existente}')
-
-    
-    resultado = chain.validate(lista_gerada, lista_existente)
+    override = ''
+    ovr_gerado = 'STEP.DDNAME DD UNIT=(SYSDA,25),STORCLAS=EXTNOST3,DATACLAS=XDEFAULT'
+    ovr_existente = 'STEP.DDNAME DD UNIT=(SYSDA,25),AVGREC=K,DATACLAS=XDEFAULT'
+    override = get_override(ovr_existente, ovr_gerado)
+    print(override)
    
-
-
-
-
-    # clas = []
-    # context.strategy = Unit_Strategy()
-
-    # context.set_data(ovr_gerado,ovr_existente)
-    # retorno = context.processa() 
-    # unit = retorno if retorno != None else ''
-
-    # context.strategy = GenericClas_Strategy()
-    # clas = context.processa()
-    # dataclas = clas[0] if clas[0] != None else ''
-    # storclas = clas[1] if clas[1] != None else ''
-
-
-    # ovr_total = ''
-
-
-
-    # ovr_total+=unit 
-    # if ovr_total != '':
-    #     if dataclas != '':
-    #         ovr_total+=','
-
-    #     ovr_total+=dataclas
-
-    #     if storclas != '':
-    #         ovr_total+=','
-
-    #     ovr_total+=storclas
-
-    # print(f'ovr_total {ovr_total}')
-
 
--- strategy/pattern_chain.py
+++ strategy/pattern_chain.py
@@ -20,7 +20,7 @@ class Chain_pattern(ABC):
         pass
 
     @abstractmethod
-    def validate(self, gerada: List=[], existente: List= [], resultado: str = None)-> str:
+    def montar_override(self, gerada: List=[], existente: List= [], resultado: str = None)-> str:
         pass
 
 class Unit_chain(Chain_pattern):
@@ -30,7 +30,7 @@ class Unit_chain(Chain_pattern):
 
     def get_parameters(self, data, retorno: List=[])-> List:
         unit = None
-        print('peguei unit')
+        print('processando unit...')
         indice = data.find('DD')
         data = data[indice+3:]
         
@@ -47,12 +47,20 @@ class Unit_chain(Chain_pattern):
 
         if self.get_proximo()  != None:
             return  self.get_proximo().get_parameters(data, retorno)
-        return retorno
+        return [retorno]
     
-    def validate(self, gerada: List=[], existente: List= [], resultado: str = None)-> str:
-        print('validei unit')
+    def montar_override(self, data: str, existente: List= [], resultado: str = None)-> str:
+        print('Gerando override com unit...')
+        ovr = ''
+        ret_unit_gerada = [x for x in data if x != None and  'UNIT' in x]
+
+        if len(ret_unit_gerada) > 0:
+            ovr+=''.join(ret_unit_gerada)
+
+        resultado+= ovr
+        # print(f'override unit {resultado}')
         if self.get_proximo()  != None:
-            return  self.get_proximo().validate(gerada, existente, resultado)
+            return  self.get_proximo().montar_override(data, existente, resultado)
         return resultado
 
 class Clas_chain(Chain_pattern):
@@ -62,32 +70,89 @@ class Clas_chain(Chain_pattern):
         retorno = None
 
     def get_parameters(self, data, retorno: List=[])-> List:
+        print('processando _clas...')
         indice = data.find('DD')
         data = data[indice+3:]
         pattern_dataclas='(DATACLAS=)(\w+)'
         pattern_storclas='(STORCLAS=)(\w+)'
+        pattern_avgrec='(AVGREC=)(\w+)'
+
         dataclas = None
         storclas = None
+        avgrec = None
 
         # match variable contains a Match object.
         match_dataclas = re.search(pattern_dataclas, data) 
         match_storclas = re.search(pattern_storclas, data) 
+        match_avgrec = re.search(pattern_avgrec, data) 
 
         if match_dataclas != None:
             dataclas = match_dataclas.group()
+            retorno.append(dataclas)
         if match_storclas != None:
             storclas = match_storclas.group()
-            
-        clas =  dataclas,storclas
-        retorno.append(clas)
-
+            retorno.append(storclas)
+        if match_avgrec != None:
+            avgrec = match_avgrec.group()
+            retorno.append(avgrec)
         if self.get_proximo()  != None:
             return  self.get_proximo().get_parameters(data, retorno)
         return retorno
 
-    def validate(self, gerada: List=[], existente: List= [], resultado: str = None)-> str:
-        print('validei clas')
+    def montar_override(self, data: str, existente: List=[], resultado: str = None)-> str:
+        print('gerando override _clas...')
+        ovr = ''
+        ret_dataclas_gerada = [x for x in data if x != None and 'DATACLAS' in x]
+        ret_dataclas_existente = [x for x in existente if x != None and 'DATACLAS' in x]
+        ret_storclas_gerada = [x for x in data if x != None and 'STORCLAS' in x]
+        ret_storclas_existente = [x for x in existente if x != None and 'STORCLAS' in x]
+        ret_avgrec_existente = [x for x in existente if x != None and 'AVGREC' in x]
+        ret_avgrec_gerada = [x for x in data if x != None and 'AVGREC' in x]
+
+        # if len(ret_dataclas_gerada) > 0:
+        #     if ovr != '' or resultado != '':
+        #         ovr+=','
+        #     ovr+=''.join(ret_dataclas_gerada)
+        # elif len(ret_dataclas_existente) > 0:
+        #     if ovr != '' or resultado != '':
+        #         ovr+=','
+        #     ovr+=''.join(ret_dataclas_existente)
+
+        if len(ret_dataclas_gerada) > 0:
+            if ovr != '' or resultado != '':
+                ovr+=','
+            ovr+='DATACLAS=XDEFCOMP' \
+                if ''.join(ret_dataclas_existente) == 'DATACLAS=XDEFCOMP' \
+                    else ''.join(ret_dataclas_gerada)
+        elif len(ret_dataclas_existente) > 0:
+            if ovr != '' or resultado != '':
+                ovr+=','
+            ovr+='DATACLAS=XDEFCOMP' \
+                if ''.join(ret_dataclas_gerada) == 'DATACLAS=XDEFCOMP' \
+                    else ''.join(ret_dataclas_existente)
+
+            
+        if len(ret_storclas_gerada) > 0:
+            if ovr != '' or resultado != '':
+                ovr+=','
+            ovr+=''.join(ret_storclas_gerada)
+        elif len(ret_storclas_existente) > 0:
+            if ovr != '' or resultado != '':
+                ovr+=','
+            ovr+=''.join(ret_storclas_existente)
+
+        if len(ret_avgrec_gerada) > 0:
+            if ovr != '' or resultado != '':
+                ovr+=','
+            ovr+=''.join(ret_avgrec_gerada)
+        elif len(ret_avgrec_existente) > 0:
+            if ovr != '' or resultado != '':
+                ovr+=','
+            ovr+=''.join(ret_avgrec_existente)
+
+        resultado+= ovr
+        # print(f'override  {resultado}')
         if self.get_proximo()  != None:
-            return  self.get_proximo().validate(gerada, existente, resultado)
+            return  self.get_proximo().montar_override(data, existente, resultado)
         return resultado
 
